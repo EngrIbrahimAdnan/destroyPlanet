aa7e8fb1319fba7a28852d323332d36e8a771f94
ibrahim.a.y.m.98@gmail.com <ibrahim.a.y.m.98@gmail.com>
2024-10-20 10:03:33 +0300

    further improvments on script

--- src/app/page.js
+++ src/app/page.js
@@ -8,34 +8,71 @@ export default function Home() {
   const [buttonClicks, setButtonClicks] = useState(0);
   const [score, setScore] = useState(0);
   const [increment, setIncrement] = useState(1);
+  const [double, setDouble] = useState(10);
+  const [triple, setTriple] = useState(20);
+  const [quad, setQuad] = useState(30);
+
+  const [WelcomeWindowStatus, setWelcomeWindowStatus] = useState(false);
 
   let current = 0;
 
+  // Function to toggle modal. This is a generic function to be used to set models ON and OFF
+  function toggleModal(setState, state) {
+    return setState(!state);
+  }
+
   const incrementButtonClicks = () => {
-    // increments the number of clicks on the site
-    // let double = 10;
-    // let triple = 20;
-    // let quad = 30;
-    if (score === 10) {
+    current = buttonClicks + increment;
+    setScore(score + increment);
+
+    setButtonClicks(current);
+    if (score === double) {
       // check if the user passed 10 score to give upgrade
       const confirmed = confirm("double your clicking power??");
       if (confirmed) {
-        setIncrement(increment + 1); //doubles the clicking points and rests score
+        setIncrement(increment * 2); //doubles the clicking points and rests score
         setScore(score - 10);
+        setDouble(double - 12);
       }
     } else {
-      //if user doesnt want the upgrade continue as normal
-      current = buttonClicks + increment;
-      setScore(score + increment);
-
-      return setButtonClicks(current);
     }
+    if (score === triple) {
+      const confirmed = confirm("TTTTTRRRRRRIIIIIPPPPPPPLLLEEE??");
+      if (confirmed) {
+        setIncrement(increment * 3);
+        setScore(score - 20);
+        setTriple(triple - 22);
+      } else {
+      }
+    }
+    if (score === quad) {
+      const confirmed = confirm(
+        "Quadrople your destruction pppppoooooooooweeerrr??"
+      );
+      if (confirmed) {
+        setIncrement(increment * 4);
+        setScore(score - 30);
+        setQuad(quad - 33);
+      }
+    }
+
+    current = buttonClicks + increment;
+    setScore(score + increment);
+
+    setButtonClicks(current);
+    if (current === 1) {
+      return toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus);
+    } else if (current >= 15) {
+      return toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus);
+    }
+
+    return;
   };
 
   return (
-    <div class="w-full h-screen bg-[rgb(7,12,28)] ">
-      <div class="flex justify-between items-start p-4 text-white  items-start p-4">
-        <div class="w-1/2">
+    <div className="w-full h-screen bg-[rgb(7,12,28)]">
+      <div className="flex justify-between items-start p-4 text-white  items-start p-4">
+        <div className="w-1/2">
           <img
             src="https://raw.githubusercontent.com/EngrIbrahimAdnan/destroyPlanet/refs/heads/main/src/app/assets/deathStar.png"
             className="w-60  "
@@ -46,7 +83,7 @@ export default function Home() {
             src="https://raw.githubusercontent.com/EngrIbrahimAdnan/destroyPlanet/refs/heads/main/src/app/assets/Money.PNG"
             className="w-20 "
           ></img>
-          <h1 class="text-2xl font-bold">X {score}</h1>
+          <h1 className="text-2xl font-bold">X {score}</h1>
         </div>
       </div>
 
@@ -56,13 +93,87 @@ export default function Home() {
           <h3 className="text-2xl text-white mb-4">Planets Destroyed</h3>
           <button
             onClick={incrementButtonClicks}
-            className="px-6 py-2 bg-blue-500 text-white rounded mb-4 hover:bg-blue-700"
+            className="px-4 py-2 mb-4 rounded text-white transition-all duration-300 bg-red-500 hover:bg-red-800 hover:opacity-100"
           >
             Destroy Planet
           </button>
           <p className="text-white">{increment} Planet per click </p>
         </div>
       </div>
+
+      {/* Welcome model */}
+      {WelcomeWindowStatus && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+          onClick={() =>
+            toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus)
+          }
+        >
+          <div
+            className="bg-white p-8 rounded-lg text-center "
+            onClick={(e) => e.stopPropagation()} // Prevent modal close when clicking inside
+          >
+            <h2 className="text-2xl font-bold mb-4">
+              Welcome to The Dark Side
+            </h2>
+            <p className="mb-4">
+              You just destroyed your first planet. Congratulations on getting
+              the Acolyte achievement.
+            </p>
+
+            <img
+              src="https://the-death-star.netlify.app/static/media/this-is-where-the-fun-begins.59146952.gif"
+              className="max-w-full pb-6 w-auto h-auto object-contain mx-auto"
+            ></img>
+
+            <button
+              className="px-4 py-2 rounded transition-all duration-300 bg-gray-200 hover:bg-gray-500 hover:opacity-75"
+              onClick={() =>
+                toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus)
+              }
+            >
+              Let the Fun Begin
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* use this as template for a different model */}
+      {WelcomeWindowStatus && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+          onClick={() =>
+            toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus)
+          }
+        >
+          <div
+            className="bg-white p-8 rounded-lg text-center "
+            onClick={(e) => e.stopPropagation()} // Prevent modal close when clicking inside
+          >
+            <h2 className="text-2xl font-bold mb-4">
+              Welcome to The Dark Side
+            </h2>
+            <p className="mb-4">
+              You just destroyed your first planet. Congratulations on getting
+              the Acolyte achievement.
+            </p>
+
+            <img
+              src="https://the-death-star.netlify.app/static/media/this-is-where-the-fun-begins.59146952.gif"
+              className="max-w-full pb-6 w-auto h-auto object-contain mx-auto"
+            ></img>
+
+            <button
+              className="px-4 py-2 rounded transition-all duration-300 bg-gray-200 hover:bg-gray-500 hover:opacity-75"
+              onClick={() =>
+                toggleModal(setWelcomeWindowStatus, WelcomeWindowStatus)
+              }
+            >
+              Let the Fun Begin
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
